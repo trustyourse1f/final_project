7a8b95c51a9c1950477a6965401e28fa7739d468
good-jinu <gooday6983@gmail.com>
2022-07-21 14:37:11 +0900

    update start_end csv and database

--- dbenv/petmily.sql
+++ dbenv/petmily.sql
@@ -3,75 +3,78 @@ CREATE DATABASE petmily_db default CHARACTER SET UTF8;
 USE petmily_db;
 DROP TABLE IF EXISTS hospital_info;
 CREATE TABLE hospital_info(
-		HospitalID INT(11) PRIMARY KEY,
-		name VARCHAR(255),
-		contract VARCHAR(255),
-        is24 TINYINT(1),
-        isBeautyParlor TINYINT(1),
-		isHotel TINYINT(1),
-        isStore TINYINT(1),
-        hasParkingLot TINYINT(1),
-        businessHours VARCHAR(255),
-        latitude DOUBLE(28, 25),
-        longitude DOUBLE(28, 25),
-        address VARCHAR(255),
-        subject VARCHAR(255),
-        species VARCHAR(255)
+    HospitalID INT(11) PRIMARY KEY,
+    name VARCHAR(255),
+    contract VARCHAR(12),
+    is24 TINYINT(1),
+    isBeautyParlor TINYINT(1),
+    isHotel TINYINT(1),
+    isStore TINYINT(1),
+    hasParkingLot TINYINT(1),
+    businessHours VARCHAR(255),
+    latitude DOUBLE(28, 25),
+    longitude DOUBLE(28, 25),
+    address VARCHAR(255),
+    subject VARCHAR(255),
+    species VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS reservation;
 CREATE TABLE reservation(
-		Hospital_ID INT(11),
-        Customer_name VARCHAR(255),
-        Customer_number VARCHAR(255),
-        AnimalType VARCHAR(31),
-        Symptom VARCHAR(255),
-        Time CHAR(5),
-        FOREIGN KEY(Hospital_ID) REFERENCES hospital_info(HospitalID)
+    Hospital_ID INT(11),
+    Customer_name VARCHAR(10),
+    Customer_number VARCHAR(14),
+    AnimalType VARCHAR(3),
+    Symptom VARCHAR(255),
+    Time VARCHAR(255),
+    FOREIGN KEY(Hospital_ID) REFERENCES hospital_info(HospitalID)
 );
 
 DROP TABLE IF EXISTS symptom;
 CREATE TABLE symptom(
-		Symptom_code VARCHAR(15),
-        Sorted_Symptom_kor VARCHAR(255),
-        Sorted_Symptom_eng VARCHAR(255),
-        Symptom_code_list VARCHAR(255),
-        Symptom_name VARCHAR(255)
+    Symptom_code VARCHAR(15),
+    Sorted_Symptom_kor VARCHAR(255),
+    Sorted_Symptom_eng VARCHAR(255),
+    Symptom_code_list VARCHAR(255),
+    Symptom_name VARCHAR(255),
+    info VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS disease_info;
 CREATE TABLE disease_info(
-		Disease_name VARCHAR(255),
-        Species VARCHAR(255),
-		Definition VARCHAR(255),
-        Main_Symptom VARCHAR(1024)
+    Disease_name VARCHAR(255),
+    Species VARCHAR(255),
+    Definition TEXT,
+    Main_Symptom TEXT
 );
 
 DROP TABLE IF EXISTS gu_info;
 CREATE TABLE gu_info(
-		gu VARCHAR(125),
-        Hospital TINYINT(15),
-        Is24 TINYINT(15)
+    gu CHAR(10),
+    Hospital TINYINT(3),
+    Is24 TINYINT(3)
 );
 
 DROP TABLE IF EXISTS start_end;
 CREATE TABLE start_end(
-		name VARCHAR(255),
-        mon_start VARCHAR(255),
-        tue_start VARCHAR(255),
-        wed_start VARCHAR(255),
-        thu_start VARCHAR(255),
-        fri_start VARCHAR(255),
-        sat_start VARCHAR(255),
-        sun_start VARCHAR(255),
-        hol_start VARCHAR(255),
-        mon_end VARCHAR(255),
-        tue_end VARCHAR(255),
-        wed_end VARCHAR(255),
-        thu_end VARCHAR(255),
-        fri_end VARCHAR(255),
-        sat_end VARCHAR(255),
-        sun_end VARCHAR(255),
-        hol_end VARCHAR(255),
-        businessHours VARCHAR(255)
+    HospitalID INT(11) PRIMARY KEY,
+    name CHAR(50),
+    mon_start CHAR(50),
+    tue_start CHAR(50),
+    wed_start CHAR(50),
+    thu_start CHAR(50),
+    fri_start CHAR(50),
+    sat_start CHAR(50),
+    sun_start CHAR(50),
+    hol_start CHAR(50),
+    mon_end CHAR(50),
+    tue_end CHAR(50),
+    wed_end CHAR(50),
+    thu_end CHAR(50),
+    fri_end CHAR(50),
+    sat_end CHAR(50),
+    sun_end CHAR(50),
+    hol_end CHAR(50),
+    businessHours CHAR(255),
+    FOREIGN KEY(HospitalID) REFERENCES hospital_info(HospitalID)
 );
--- dbenv/sql.py
+++ dbenv/sql.py
@@ -2,12 +2,12 @@ import pymysql
 import csv
 import pandas as pd
 
-hostname = ''
+hostname = 'localhost'
 portnum = 3306
-pswd = ''
+pswd = '48615+'
 
 ###petraschu
-conn = pymysql.connect(host = hostname, port = portnum, user='root',password=pswd,db='petmily_db',charset='utf8')
+conn = pymysql.connect(host = hostname, port = portnum, user='root',password='pswd',db='petmily_db',charset='utf8')
 curs = conn.cursor()
 sql = "insert into hospital_info (HospitalID, name, contract, is24, isBeautyParlor, isHotel, isStore, hasParkingLot, businessHours, latitude, longitude, address, subject, species) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 f = open('./dataset/petraschu.csv','r',encoding='cp949')
@@ -20,21 +20,21 @@ f.close()
 
 ###disease_info
 sql = "insert into disease_info (Disease_name, Species, Definition, Main_Symptom) values (%s,%s,%s,%s)"
-f = open('./dataset/질병정보v3.csv','r',encoding='cp949')
+f = open('./dataset/질병정보v5.csv','r',encoding='cp949')
 rd = csv.reader(f)
 next(rd)
 for line in rd:
-    curs.execute(sql, (line[0],line[1],line[2],line[3]))
+    curs.execute(sql, (line[0],line[1],line[2],line[4]))
 conn.commit()
 f.close()
 
 ###symptom
-sql = "insert into symptom (Symptom_code, Sorted_Symptom_kor, Sorted_Symptom_eng, Symptom_code_list, Symptom_name) values (%s,%s,%s,%s,%s)"
+sql = "insert into symptom (Symptom_code, Sorted_Symptom_kor, Sorted_Symptom_eng, Symptom_code_list, Symptom_name, info) values (%s,%s,%s,%s,%s,%s)"
 f = open('./dataset/동물질병 증상분류.csv','r',encoding='cp949')
 rd = csv.reader(f)
 next(rd)
 for line in rd:
-    curs.execute(sql, (line[0],line[1],line[2],line[3],line[4]))
+    curs.execute(sql, (line[0],line[1],line[2],line[3],line[4],line[5]))
 conn.commit()
 f.close()
 
@@ -49,12 +49,12 @@ conn.commit()
 f.close()
 
 ###start_end
-sql = "insert into start_end (name, mon_start, tue_start, wed_start, thu_start, fri_start, sat_start, sun_start, hol_start, mon_end, tue_end, wed_end, thu_end, fri_end, sat_end, sun_end, hol_end, businessHours) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sql = "insert into start_end (HospitalID, name, mon_start, tue_start, wed_start, thu_start, fri_start, sat_start, sun_start, hol_start, mon_end, tue_end, wed_end, thu_end, fri_end, sat_end, sun_end, hol_end, businessHours) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 f = open('./dataset/start_endv1.csv','r',encoding='utf-8')
 rd = csv.reader(f)
 next(rd)
 for line in rd:
-    curs.execute(sql, (line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],line[16],line[17]))
+    curs.execute(sql, (line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],line[16],line[17],line[18]))
 conn.commit()
 conn.close()
 f.close()
