3a3a10192a5888a262b0abd1e83a99ef0a885620
good-jinu <gooday6983@gmail.com>
2022-07-22 17:25:32 +0900

    feat: add time-selection

--- client/src/components/section/infosection/Reservation.js
+++ client/src/components/section/infosection/Reservation.js
@@ -6,29 +6,33 @@ import 'assets/CSS/Calendar.css';
 function Reservation(props) {
     const [selectedDate, setSelectedDate] = useState(new Date());
     let hname = null;
-    //let reserveContainer = [];
+    let timeSelectionBtns = [];
     hname = props.name;
 
     useEffect(() => {
         get_buisnesshour(props.HospitalID);
     }, [])
 
-    /*
-    for(let i=9; i<18; i++)
+    for(let i=18; i<36; i++)
     {
-        let clickbtn = (e) => {
+        const hour = parseInt(i/2);
+        const minute = (i % 2) * 30;
+        selectedDate.setHours(hour);
+        selectedDate.setMinutes(minute);
+        const tstmp = selectedDate.getTime();
+        const clickbtn = (e) => {
             let data = {
                 HospitalID: props.HospitalID,
                 Customer_name:'이진우',
                 Customer_number:'010-1234-1234',
                 AnimalType: '개',
                 Symptom: '결막염',
-                Time: '10:00'
+                Time: tstmp
             };
             post_reservation(data);
         }
-        reserveContainer.push(<div><button onClick={clickbtn}>{`${i}시 예약`}</button></div>);
-    }*/
+        timeSelectionBtns.push(<button onClick={clickbtn}>{`${hour}:${minute}`}</button>);
+    }
     
     return (
         <div>
@@ -38,11 +42,10 @@ function Reservation(props) {
             </div>
             <div className="time-selection-container">
                 <h1>
-                    {`${selectedDate.getFullYear()}년 ${selectedDate.getMonth()}월 ${selectedDate.getDate()}일`}
+                    {`${selectedDate.getFullYear()}년 ${selectedDate.getMonth() + 1}월 ${selectedDate.getDate()}일`}
                 </h1>
                 <div className="time-selection">
-                    <button>9:00</button>
-                    <button>9:30</button>
+                    {timeSelectionBtns}
                 </div>
             </div>
         </div>
