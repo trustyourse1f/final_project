6da5d73014e43977d1bc8db9e3bd969be00763b9
good-jinu <gooday6983@gmail.com>
2022-07-28 10:15:01 +0900

    update style sheets
    remove dark mode button

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,12 @@ class App extends React.Component {
   constructor(props)
   {
     super(props);
-    this.state = {
-      classname: "light_mode"
-    };
   }
 
   render() {
     return (
-      <div className={this.state.classname} id="App">
-        <Header thememode_callback={() => {
-          if(this.state.classname === "light_mode")
-          {
-            this.setState({ classname: "dark_mode"});
-          }
-          else if(this.state.classname === "dark_mode")
-          {
-            this.setState({classname: "light_mode"});
-          }
-        }}/>
+      <div id="App">
+        <Header/>
         <MainSection/>
       </div>
     );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,34 +5,13 @@ class Header extends React.Component {
   constructor(props)
   {
     super(props);
-    this.state = {
-      "btn_name": "Light",
-      "isDark": false
-    };
-    this.handle_thememode = this.handle_thememode.bind(this);
-  }
-
-  handle_thememode(e) {
-    this.props.thememode_callback();
-    if (this.state.isDark) {
-      this.setState({
-        "btn_name": "Light",
-        "isDark": false,
-      });
-    } else {
-      this.setState({
-        "btn_name": "Dark",
-        "isDark": true
-      });
-    }
   }
   
   render() {
     return (
         <header id="header">
-            <h1>동물병원 예약시스템</h1>
+            <h1><img src='/Images/1-logo.png'/>동물병원 예약시스템</h1>
             <nav>
-              <button onClick={this.handle_thememode} id="darkmode_btn">{this.state.btn_name}</button>
               <a href="https://github.com/good-jinu/petmily" target="_blank" rel="noopener noreferrer">
                 <img src="/GitHub-Mark-32px.png" className="mark" alt="github"/>
               </a>
