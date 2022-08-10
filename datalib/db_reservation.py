2c48d450b0315d282a715ef71ff6f255136cdb44
SeongBin <tjdqls0716@gmail.com>
2022-08-10 15:58:55 +0900

    예약정보 병원에 전송

--- datalib/db_reservation.py
+++ datalib/db_reservation.py
@@ -13,9 +13,12 @@ def user_reservation(db,hospiID):
 
 def host_reservation(db,hospiID):
     query = f'SELECT * FROM reservation WHERE Hospital_ID = "{hospiID}"'
+    query1 = f'SELECT name FROM hospital_info WHERE HospitalID = "{hospiID}"'
     result=[]
     c1=db.cursor()
     c1.execute(query)
+    c2=db.cursor()
+    c2.execute(query1)
     for row in c1:
         j_dict = {
             "Customer_name":row[2],
@@ -25,4 +28,10 @@ def host_reservation(db,hospiID):
             "Time":row[6]
                 }
         result.append(j_dict)
-    return result
+    for i in c2:
+        result_dict = {
+            'name':i,
+            'reservations':result
+        }
+    
+    return result_dict
